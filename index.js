7234295eb0806a370341a96e656ac42addad8ac3
Adrián Siñiga Meléndez <adrian.siniga@bicevida.cl>
2020-10-18 20:38:52 -0300

    configuración mongoose y dot env

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const { ApolloServer,  gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+
+const conectarDB = require('./config/db');
+
+// Conectar a la base de datos
+conectarDB();
 
 //servidor
-const server = new ApolloServer();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 //arrancar el servidor
 server.listen().then(({url}) => {
